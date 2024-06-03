b1e3f80d4b51eca2fdfce32b84234ca59533f65f
YasserTassa <yhajo5234@gmail.com>
2024-06-03 09:59:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ console.log(categories);
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       console.log(chosenCategoryShortName);
-      var homeHtmlToInsertIntoMainPage = 'Section class="row"'+insertProperty(homeHtml,"randomCategoryShortName","chosenCategoryShortName")+"</section>";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName","chosenCategoryShortName");
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
